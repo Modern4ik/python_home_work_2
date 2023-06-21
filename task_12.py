e00b40537f17b5641a812bf5d450361d98e294fc
Sergey Zaytsev <surgery_333@mail.ru>
2023-06-21 13:26:02 +0300

    Task12 solved and file task_12.py with solution added

---
+++ task_12.py
@@ -0,0 +1,20 @@
+numb_sum = int(input("Введите cумму для загаданных чисел: "))
+numb_prod = int(input("Введите произведение для загаданных чисел: "))
+
+first_digit = numb_sum
+second_digit = 0
+flag = True
+
+while flag:
+    first_digit -= 1
+    second_digit = numb_sum - first_digit
+    
+
+    if first_digit * second_digit == numb_prod or first_digit * second_digit == 0:
+        flag = False
+
+
+if first_digit != 0:
+    print(f"Загаданные числа: {first_digit} и {second_digit}")
+else:
+    print(f"Так не бывает")
